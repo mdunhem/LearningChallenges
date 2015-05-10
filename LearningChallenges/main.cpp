c7754a7e3804aa77f50f56694130c66127d7df68
Mikael Dunhem <mikael.dunhem@gmail.com>
2015-05-09 21:25:09 -0400

    Initial Commit

---
+++ LearningChallenges/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearningChallenges
+//
+//  Created by Sven on 5/9/15.
+//  Copyright (c) 2015 Mikael Dunhem. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
