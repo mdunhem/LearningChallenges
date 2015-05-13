79cec301d3297090b4e576b954011c5593e7082b
Mikael Dunhem <mikael.dunhem@gmail.com>
2015-05-13 00:22:19 -0400

    Changes the tester for the reverse print linked list
    This adds the ability for the user to enter in numbers into the linked list.
    Nothing that special.

--- LearningChallenges/ReversePrintLinkedList/ReversePrintLinkedListTest.cpp
+++ LearningChallenges/ReversePrintLinkedList/ReversePrintLinkedListTest.cpp
@@ -6,6 +6,9 @@
 //  Copyright (c) 2015 Mikael Dunhem. All rights reserved.
 //
 
+#include <iostream>
+#include <sstream>
+#include <vector>
 #include "ReversePrintLinkedListTest.h"
 #include "LinkedList.h"
 
@@ -15,23 +18,32 @@ namespace ReversePrintLinkedList {
     
     void ReversePrintLinkedListTest::run() {
         LinkedList linkedList;
+        std::string input;
+        
         
         std::cout << "Prints out a linked list in reverse.\n\n";
+        std::cout << "Enter numbers to add to the list (only integers): ";
+        std::getline(std::cin, input);
         
-        linkedList.add(1);
-        linkedList.add(0);
-        linkedList.add(3);
-        linkedList.add(2);
-        linkedList.add(3);
-        linkedList.add(4);
+        // This parses the getline into individual numbers
+        // Cleanest solution I could come up with
+        std::istringstream stringStream(input);
+        std::vector<int> tokens {
+            std::istream_iterator<int> {stringStream},
+            std::istream_iterator<int> {}
+        };
+        
+        for (int &i : tokens) {
+            linkedList.add(i);
+        }
         
         linkedList.nonDestructivelyIterate(&ReversePrintLinkedListTest::iterateLinkedList);
         
-        std::cout << output;
+        std::cout << "\n\n" << output;
     }
     
      void ReversePrintLinkedListTest::iterateLinkedList(int value) {
-        output.insert(0, std::to_string(value));
+        output.insert(0, std::to_string(value) + " ");
     }
     
 }
